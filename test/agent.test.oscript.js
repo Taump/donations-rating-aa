6551c56f620d060496e6b6514b2a36d7d1e497d5
Taump <taump@yandex.ru>
2023-11-09 17:53:18 +0400

    test: reg symbols

--- test/agent.test.oscript.js
+++ test/agent.test.oscript.js
@@ -10,9 +10,13 @@ describe('Check simple AA', function () {
 	before(async () => {
 		this.network = await Network.create()
 			.with.numberOfWitnesses(1)
+			.with.asset({ eth: {} })
+			.with.asset({ usdc: {} })
+			.with.agent({ tokenRegistry: path.join(__dirname, '../node_modules/registry-aa/token-registry.oscript') })
 			.with.agent({ rating: path.join(__dirname, AA_PATH) })
-			.with.wallet({ alice: 1e6 })
-			.with.wallet({ bob: 1e3 })
+			.with.wallet({ oracle: { base: 1e9 } })
+			.with.wallet({ alice: 100e9 })
+			.with.wallet({ bob: 100e9 })
 			.run();
 
 		this.alice = this.network.wallet.alice;
@@ -20,7 +24,11 @@ describe('Check simple AA', function () {
 		this.bob = this.network.wallet.bob;
 		this.bobAddress = await this.bob.getAddress();
 
+		this.tokenRegistryAddress = this.network.agent.tokenRegistry;
 		this.ratingAddress = this.network.agent.rating;
+
+		this.usdcAsset = this.network.asset.usdc;
+		this.ethAsset = this.network.asset.eth;
 	})
 
 	it('Issue rating asset', async () => {
@@ -44,7 +52,66 @@ describe('Check simple AA', function () {
 		expect(vars.supply).to.be.eq(0);
 		expect(vars.asset).to.be.eq(this.ratingAsset);
 
-	}).timeout(60000)
+	}).timeout(60000);
+
+
+	it('Register USDC and ETH', async () => {
+		const USDCSymbol = 'USDC';
+		const ETHSymbol = 'ETH';
+
+		// register USDC
+		const { unit, error } = await this.network.wallet.alice.triggerAaWithData({
+			toAddress: this.tokenRegistryAddress,
+			amount: 1e8,
+			data: {
+				symbol: USDCSymbol,
+				asset: this.usdcAsset,
+				decimals: 4,
+				description: "USDC token for testing",
+			}
+		})
+
+		expect(error).to.be.null;
+		expect(unit).to.be.validUnit;
+
+		const { response } = await this.network.getAaResponseToUnitOnNode(this.network.wallet.alice, unit);
+
+		const drawer_key = this.aliceAddress + '_' + 0 + '_' + USDCSymbol + '_' + this.usdcAsset;
+		expect(response.response.responseVars.message).to.be.equal('Your description is now the current');
+		expect(response.response.responseVars[USDCSymbol]).to.be.equal(this.usdcAsset);
+		expect(response.response.responseVars[this.usdcAsset]).to.be.equal(USDCSymbol);
+		expect(response.response.responseVars[drawer_key]).to.be.equal(1e8);
+
+		const { vars } = await this.alice.readAAStateVars(this.tokenRegistryAddress);
+		expect(vars[`a2s_${this.usdcAsset}`]).to.be.eq("USDC");
+
+
+		// reg ETH
+		const { unit: unit2, error: error2 } = await this.network.wallet.alice.triggerAaWithData({
+			toAddress: this.tokenRegistryAddress,
+			amount: 1e8,
+			data: {
+				symbol: ETHSymbol,
+				asset: this.ethAsset,
+				decimals: 9,
+				description: "ETH token for testing",
+			}
+		})
+
+		expect(error2).to.be.null;
+		expect(unit2).to.be.validUnit;
+
+		const { response: response2 } = await this.network.getAaResponseToUnitOnNode(this.network.wallet.alice, unit2);
+
+		const drawer_key2 = this.aliceAddress + '_' + 0 + '_' + ETHSymbol + '_' + this.ethAsset;
+		expect(response2.response.responseVars.message).to.be.equal('Your description is now the current');
+		expect(response2.response.responseVars[ETHSymbol]).to.be.equal(this.ethAsset);
+		expect(response2.response.responseVars[this.ethAsset]).to.be.equal(ETHSymbol);
+		expect(response2.response.responseVars[drawer_key2]).to.be.equal(1e8);
+
+		const { vars: vars2 } = await this.alice.readAAStateVars(this.tokenRegistryAddress);
+		expect(vars2[`a2s_${this.ethAsset}`]).to.be.eq("ETH");
+	}).timeout(60000);
 
 	after(async () => {
 		await this.network.stop()
